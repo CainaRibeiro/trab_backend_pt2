4c3a3ac7c741d291d625aa652b01efc17fa3130d
Caina Ribeiro <cainaribeiro.it@outlook.com>
2024-11-03 20:50:28 -0300

    users test case

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,3 +1,17 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
+from .models import User
 
-# Create your tests here.
+class UserTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(name="user", email="user@example.com")
+
+
+    def test_get_users(self):
+        response = self.client.get(reverse('get-users'))
+        response_data = json.loads(response.content.decode())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response_data), 1)
+        self.assertEqual(response_data[0]["name"], "user")
